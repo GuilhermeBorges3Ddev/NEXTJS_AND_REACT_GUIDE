92bb375dd4dc21c7bf520d5b70f748e9402af272
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-02-28 23:59:30 -0300

    App.js redone with class instead function

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,27 +7,29 @@ const USER_INFO = {
   userIsLogged: false,
 };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {USER_INFO.userIsLogged
-            ? `Hello ${USER_INFO.username}`
-            : "You're not logged"}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {USER_INFO.userIsLogged
+              ? `Hello ${USER_INFO.username}`
+              : "You're not logged"}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
