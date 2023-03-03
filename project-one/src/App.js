e69edebb213b495180874bf3254b39f32c75d8bc
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-03-03 00:39:37 -0300

    App.js all on class components and using state with constructor

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -8,7 +8,24 @@ const USER_INFO = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Guilherme S. Borges",
+      counter: 1,
+    };
+  }
+  handleParagraphClick() {
+    this.setState({
+      name: "Paragraph clicked, name deleted and changed to this phrase",
+    });
+  }
+  handleLinkClick = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
   render() {
+    const { name, counter } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -18,11 +35,17 @@ class App extends Component {
               ? `Hello ${USER_INFO.username}`
               : "You're not logged"}
           </p>
+          <p onClick={() => this.handleParagraphClick()}>Author: {name}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={(e) => {
+              e.preventDefault();
+              this.handleLinkClick();
+              alert(`Counter of link clicks: ${counter}`);
+            }}
           >
             Learn React
           </a>
