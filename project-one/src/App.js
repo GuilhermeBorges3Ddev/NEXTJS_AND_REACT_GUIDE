05f230564639df794360b44fb44991b9a1dba465
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-03-04 00:15:18 -0300

    RCC on App.js, version 2

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,55 +1,42 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-const USER_INFO = {
-  username: "Admin Hexanderson",
-  userIsLogged: false,
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Guilherme S. Borges",
-      counter: 1,
+      posts: [
+        {
+          id: 1,
+          title: "Quis dolor officia",
+          body: "The body one",
+        },
+        {
+          id: 2,
+          title: "Labore ut proident",
+          body: "The body two",
+        },
+        {
+          id: 3,
+          title: "Laborum aliqua officia",
+          body: "The body three",
+        },
+      ],
     };
   }
-  handleParagraphClick() {
-    this.setState({
-      name: "Paragraph clicked, name deleted and changed to this phrase",
-    });
-  }
-  handleLinkClick = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  };
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {USER_INFO.userIsLogged
-              ? `Hello ${USER_INFO.username}`
-              : "You're not logged"}
-          </p>
-          <p onClick={() => this.handleParagraphClick()}>Author: {name}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => {
-              e.preventDefault();
-              this.handleLinkClick();
-              alert(`Counter of link clicks: ${counter}`);
-            }}
+        {posts.map((postItem) => (
+          <div
+            key={postItem.id}
+            style={{ border: "1px solid black", margin: "1rem 0" }}
           >
-            Learn React
-          </a>
-        </header>
+            <h1>{postItem.title}</h1>
+            <p>{postItem.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
