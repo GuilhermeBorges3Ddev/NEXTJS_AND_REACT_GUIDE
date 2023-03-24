d0e5e331f52e48d2125a2755bce6db7acfce51f3
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-03-24 01:09:46 -0300

    Home can be used as a function or a class, depends of what programmer pass as prop to Home component

--- project-one/src/index.js
+++ project-one/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./templates/Home";
+import Home from "./templates/Home";
 import "./styles/global-styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Home type="function" />
   </React.StrictMode>
 );
--- project-one/src/templates/Home/index.jsx
+++ project-one/src/templates/Home/index.jsx
@@ -1,11 +1,76 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Posts } from "../../components/Posts";
 import { TextInput } from "../../components/TextInput";
 import { loadPosts } from "../../utils/load-posts";
 import "./styles.css";
 
-export class Home extends Component {
+export default function Home({ type }) {
+  if (type === "function") return <HomeFunction />;
+  return <HomeClass />;
+}
+
+function HomeFunction() {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(5);
+  const [searchValue, setSearchValue] = useState("");
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue
+    ? allPosts.filter((postItem) => {
+        return postItem.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+    setPosts(posts);
+    setPage(nextPage);
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  return (
+    <section className="container">
+      <div className="search-container">
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+        {!!searchValue && <h1>Searched value: {searchValue}</h1>}
+      </div>
+      <br />
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+      {filteredPosts.length === 0 && <p>No posts found :(</p>}
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load more posts"
+            loadMorePostsAction={loadMorePosts}
+            isDisabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+  );
+}
+
+class HomeClass extends Component {
   constructor(props) {
     super(props);
     this.state = {
