8231939b07b5ef52c550a64d6a87673ecdd4dd7b
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-03-28 23:45:35 -0300

    Component PostCard has his own tests too

---
+++ project-one/src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { PostCard } from ".";
+import { PostCardPropsMock } from "./mock";
+
+const props = PostCardPropsMock;
+
+describe("<PostCard />", () => {
+  it("should render PostCard correctly", () => {
+    /* 
+        If you wanna debug:
+            const { debug } = render(<PostCard {...PostCardPropsMock} />);
+            debug();
+    */
+    render(<PostCard {...props} />);
+    const headingText = `${props.title} - ${props.id}`;
+    const imageElements = screen.getAllByRole("img", { name: props.title });
+    const headingElements = screen.getAllByRole("heading", {
+      name: headingText,
+    });
+    const paragraphText = screen.getByText(props.body);
+    expect.assertions(3);
+    expect(imageElements[0]).toHaveAttribute("src", props.cover);
+    expect(headingElements[0]).toBeInTheDocument();
+    expect(paragraphText).toBeInTheDocument();
+  });
+});
