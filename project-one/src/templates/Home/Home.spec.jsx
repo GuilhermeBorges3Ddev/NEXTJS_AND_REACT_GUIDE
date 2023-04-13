6b9ce6e210d700af15ab596092c0d4ad3178e818
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-04-12 23:45:23 -0300

    msw installed and used on Home.spec.jsx to setupServer intercepting our request

--- project-one/src/templates/Home/Home.spec.jsx
+++ project-one/src/templates/Home/Home.spec.jsx
@@ -1,5 +1,56 @@
+import React from 'react';
+import Home from '.';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title 1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title 2',
+          body: 'body2',
+          url: 'img2.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title 3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  test('dummy test to test the suit', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render search, posts and load more', async () => {
+    render(<Home type="function" />);
+    const noMorePosts = screen.getByText('No posts found :(');
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
