6e3c5114856b2d93e43c7388066bda8e80076d2f
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-08-24 00:33:35 -0300

    404 route incremented with useLocation and useHistory behaviors

--- react-router-dom-v5/src/templates/Page404/index.jsx
+++ react-router-dom-v5/src/templates/Page404/index.jsx
@@ -1,7 +1,76 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
 export const Page404 = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+  const [secondsToBeRedirect, setSecondsToBeRedirect] = useState(10);
+  useEffect(() => {
+    if (secondsToBeRedirect > 0)
+      setTimeout(() => setSecondsToBeRedirect((lastTime) => --lastTime), 1000);
+    else history.push('/');
+  }, [secondsToBeRedirect, history]);
   return (
-    <div>
-      <h1>Erro 404 - Página não encontrada</h1>
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'indianred',
+        color: 'white',
+        height: '90vh',
+      }}
+    >
+      <h1
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          width: '100vw',
+          height: '20vh',
+        }}
+      >
+        <span style={{ fontSize: '3rem', width: '100%', textAlign: 'center' }}>
+          &#128071;
+        </span>
+        <p style={{ width: '100%', textAlign: 'center' }}>
+          Error 404 - Page not found
+        </p>
+        <p style={{ width: '100%', textAlign: 'center' }}>
+          YOU WILL BE REDIRECT TO HOME PAGE IN {secondsToBeRedirect}...
+        </p>
+      </h1>
+      <h2
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          border: '5px dashed white',
+          padding: '2rem',
+        }}
+      >
+        <span style={{ width: '100%', textAlign: 'center' }}>
+          <u>We have only the following routes:</u>
+        </span>{' '}
+        <span style={{ width: '100%', textAlign: 'center', color: 'purple' }}>
+          /
+        </span>
+        <span style={{ width: '100%', textAlign: 'center', color: 'purple' }}>
+          /products/some-category
+        </span>{' '}
+        <span style={{ width: '100%', textAlign: 'center', color: 'purple' }}>
+          /products/some-category/some-id
+        </span>{' '}
+        <span style={{ width: '100%', textAlign: 'center', marginTop: '2rem' }}>
+          <u>And you've typed the following route:</u>
+        </span>{' '}
+        <span style={{ width: '100%', textAlign: 'center', color: 'purple' }}>
+          {pathname}
+        </span>{' '}
+      </h2>
     </div>
   );
 };
