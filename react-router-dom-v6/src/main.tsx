4c688705ca3337ca799a37e464a4bbbfa12b4bfd
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-08-26 23:00:44 -0300

    Basic two routes routed inside route, which is inside routes component

--- react-router-dom-v6/src/main.tsx
+++ react-router-dom-v6/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home/index.tsx";
+import { About } from "./components/About/index.tsx";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
