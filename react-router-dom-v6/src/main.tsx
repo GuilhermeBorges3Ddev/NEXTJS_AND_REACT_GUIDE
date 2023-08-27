7a87fc2cc6319f083553cd7c80944ab37795a944
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2023-08-26 23:10:20 -0300

    Menu component to demo app created and used

--- react-router-dom-v6/src/main.tsx
+++ react-router-dom-v6/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Menu } from "./components/Menu/index.tsx";
 import { Home } from "./components/Home/index.tsx";
 import { About } from "./components/About/index.tsx";
 import "./styles/global.css";
@@ -8,6 +9,7 @@ import "./styles/global.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
